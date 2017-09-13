49648d8e51df5d5cd3bb8eb6d3396a73e2a6c395
ganjun <ganjun_ec@163.com>
2017-09-13 10:59:01 +0800

    增加了对中文文本的分词功能

--- README.md
+++ README.md
@@ -5,4 +5,11 @@
 2. 负责mongodb数据库操作的mongodb.py
 3. 负责处理豆瓣书籍数据对象的douban_book.py
 4. 负责整理评论信息的用作语料库的text_presolve.py
-5. 用于对文本分析词向量的word2vec.py
+5. 负责利用jieba分词工具对原文本文件进行分词的participal.py
+6. 用于对文本分析词向量的word2vec.py
+
+## 资源(resource)模块介绍
+1. initial_text放入的是初始的未经过分词的文本
+2. participal_text放入的是经过分词后的中文文本
+3. word2vec_model保存的是word2vec过程保存下来的一些模型,方便之后加载
+4. word_dictionary保存的是自定义的一些词典(用于更精确分词)的文本
---
+++ participal.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+
+import jieba
+import jieba.posseg as pseg
+from jieba import analyse
+import codecs
+
+# 读取文章内容
+def read_article(filename):
+    f_in = codecs.open(filename, 'r', 'utf-8')
+    lines = f_in.readlines()
+    comments = u""
+    for line in lines:
+        comments += line
+    return comments
+
+# 对input_file进行分词,将中文分词好的内容输出到output_file中
+def text_participle(input_file, output_file):
+    f_in = codecs.open(input_file, 'r', 'utf-8')
+    f_out = codecs.open(output_file, 'w', 'utf-8')
+    lines = f_in.readlines()
+    comments = u""
+    for line in lines:
+        comments += line
+
+    # 精确模式
+    seg_list = jieba.cut(comments, cut_all=False)
+    f_out.write(" ".join(seg_list))
+
+    f_in.close()
+    f_out.close()
+
+# 对text文本内容提取前topk个关键词
+def text_keywords_extract(text, topk):
+     #获取关键词
+    tags = analyse.extract_tags(text, topK=topk)
+    return tags
+
+# 载入词典,增加的词典文件均放在resource/word_dictionary文件下
+def add_dictionary(dic):
+    jieba.load_userdict(dic)
+
+# 对text词性标注
+def part_of_speech(text):
+    words = pseg.cut(text, HMM=True)
+    # 每一个元素中包含了词语word和词性flag两个量
+    # for word, flag in words:
+    #     print('%s %s' % (word, flag))
+    return words
+
+# 下方是对本脚本的测试代码
+add_dictionary('resource/word_dictionary/comment_dic.txt')
+text_participle('resource/initial_text/douban_comments.txt', 'resource/participal_text/douban_comments_participle.txt')
+
